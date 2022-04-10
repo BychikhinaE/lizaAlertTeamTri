11fe76b281a9b6e2ffd537a7a4744e8dbfe6a6b1
Max Tishenko <rd-max@ya.ru>
2022-04-10 12:16:12 +0300

    fix slider

--- lessonPage.js
+++ lessonPage.js
@@ -1,14 +1,36 @@
 // SIDEBAR
-const buttonSidebar = document.querySelectorAll(".sidebar__button");
+const buttonSidebar = document.querySelectorAll(".sidebar__button"); // Все кнопки для слайдера
 
-
-buttonSidebar.forEach((element) =>
+buttonSidebar.forEach((element) => {
+  const el = element.closest("li");
+  const elIco = el.querySelector(".sidebar__ico-button"); // иконка
+  const elPanel = el.querySelector(".sidebar__panel"); //панель
+  let heihtEl = elPanel.scrollHeight + "px"; // определяем высоту контента в слайдере
+  // проверяем если панель открыта то назначаем высоту контента
+  if (
+    el
+      .querySelector(".sidebar__panel")
+      .classList.contains("sidebar__panel_opened")
+  ) {
+    el.querySelector(".sidebar__panel_opened").style.height = heihtEl;
+  }
+// вешаем слушатели на все кнопки
   element.addEventListener("click", function () {
-    const el = element.closest("li");
-    const elIco = el.querySelector(".sidebar__ico-button");
-    const elPanel = el.querySelector(".sidebar__panel");
-    elIco.classList.toggle("sidebar__ico-button_rotated");
-    elPanel.classList.toggle("sidebar__panel_opened");
-  })
-);
+    elIco.classList.toggle("sidebar__ico-button_rotated"); // меняем иконку при клике
+// проверяем если панель
+//если  открыта меняем высоту контента на 0 и закрываем его
+//если закрыта открываем панель и задаем высоту контента
 
+    if (
+      el
+        .querySelector(".sidebar__panel")
+        .classList.contains("sidebar__panel_opened")
+    ) {
+      el.querySelector(".sidebar__panel_opened").style.height = "0";
+      elPanel.classList.remove("sidebar__panel_opened");
+    } else {
+      elPanel.classList.add("sidebar__panel_opened");
+      el.querySelector(".sidebar__panel_opened").style.height = heihtEl;
+    }
+  });
+});
